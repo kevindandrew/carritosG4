1b1af9c7d82dc63934d5c55567a1b37787938e34
kevindandrew <kevindandrew77@gmail.com>
2025-07-25 11:37:41 -0400

    mejorar el diseño de las tarjetas de productos y actualizar estilos CSS

--- cards.js
+++ cards.js
@@ -104,21 +104,33 @@ let cardsArray = [
 for (let i = 0; i < cardsArray.length; i++) {
   const element = cardsArray[i];
   cards.innerHTML += `
-
-<div class="max-w-sm bg-white border border-gray-200 rounded-lg shadow-sm dark:bg-gray-800 dark:border-gray-700">
-    <a href="#">
-        <img class="rounded-t-lg" src="${cardsArray[i].image.mobile}" alt="${cardsArray[i].name}" />
-    </a>
-    <div class="p-5">
-        <a href="#">
-            <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">${cardsArray[i].name}</h5>
-        </a>
-        <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">${cardsArray[i].category} $ ${cardsArray[i].price}</p>
-        <a href="#" class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
-            Add to the Cart
-            
-        </a>
-    </div>
-</div>
+<div
+            class="w-60 rounded-xl bg-[#fef8f5] shadow-md p-3 font-sans lg:hover:shadow-lg transition"
+          >
+            <div class="rounded-xl overflow-hidden">
+              <img
+                src="${cardsArray[i].image.desktop}"
+                alt="Waffle with Berries"
+                class="w-full h-auto"
+              />
+            </div>
+            <div class="-mt-5 flex justify-center z-10">
+              <button
+                class="bg-white text-[#ec6d47] border border-[#ec6d47] px-4 py-1.5 rounded-full text-sm font-semibold hover:bg-[#fff2ee] transition flex items-center gap-2 shadow-md"
+              >
+                <img
+                  src="./assets/images/icon-add-to-cart.svg"
+                  alt="cart icon"
+                  class="h-4 w-4"
+                />
+                Añadir al Carrito
+              </button>
+            </div>
+            <p class="mt-4 text-xs text-gray-500">${cardsArray[i].category}</p>
+            <p class="text-sm font-semibold text-gray-800">
+              ${cardsArray[i].name}
+            </p>
+            <p class="text-[15px] font-bold text-[#ea6950]">$${cardsArray[i].price}</p>
+          </div>
 `;
 }
